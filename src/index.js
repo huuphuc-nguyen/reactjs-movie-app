0744e9fbd84c69af41395f4ff9c92169f03ed63e
Huu Phuc Nguyen <nhphuc236@gmail.com>
2025-01-09 19:32:56 -0600

    change base name

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from './features/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Suspense>
-    <BrowserRouter>
+    <BrowserRouter basename=".">
       <Provider store={store}>
         <App />
       </Provider>
